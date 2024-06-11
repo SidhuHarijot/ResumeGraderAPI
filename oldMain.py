36847ff4a7f3e133da3ae7acf9e8665391d03585
Harijot Singh <125294510+SidhuHarijot@users.noreply.github.com>
2024-06-10 19:42:44 -0600

    Moved code to Old Main.py

--- oldMain.py
+++ oldMain.py
@@ -2173,7 +2173,7 @@ async def create_temp_file(content: str, suffix: str) -> UploadFile:
     return UploadFile(filename=temp_file_path.name, file=open(temp_file_path, 'rb'))
 
 async def main():
-    api_key = "sk-proj-vTimyvG8rF6ohy4WrrPQT3BlbkFJzGNzjSpHkSNehZbugITH"
+    
     uid = ""
 
     # Test with TXT file
