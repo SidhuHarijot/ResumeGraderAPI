f1559f7d19bb1ad23e903b38278a36c72cabaed9
Harijot Singh <125294510+SidhuHarijot@users.noreply.github.com>
2024-06-26 19:43:18 -0600

    Resolved Logging error in resume database

--- factories.py
+++ factories.py
@@ -9,7 +9,7 @@ def log(message, func):
     Logger.logFactories(message, func)
 
 def logError(log, e: Exception, func):
-    error_message = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
+    error_message = "".join(traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__))
     message = f"{log}: {str(e)}\n{error_message}"
     Logger.logFactories(message, func, "ERROR")
 
